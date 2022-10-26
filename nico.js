093885a7fd014306a8e7670c5a13537142bf1e0c
Niconsin <nomatgroup@gmail.com>
2022-10-26 07:05:08 -1200

    Codes added

---
+++ nico.js
@@ -0,0 +1,27 @@
+
+//function learnMore() {
+  //  document.getElementById(button).innerText = "Processing"
+
+    //do
+//}
+
+
+
+//let onClick = document.getElementById(button).innerText
+
+//document.getElementById(button).innerText = "Processing"
+
+//function learnMore() {
+
+  //  console.log(onClick)
+//}
+
+let count = Processing
+
+let onClick = document.getElementById(button)
+
+function learnMore() {
+
+    onClick.innerText = count
+    console.log(count)
+}
